4d6a7a8c506991f67d28f4405dbc20e78eef3231
Aditya yadav <125307797+Doit-Ay@users.noreply.github.com>
2025-07-08 14:47:26 +0530

    Update ForumPostController.java

--- src/main/java/com/oceansense/controller/ForumPostController.java
+++ src/main/java/com/oceansense/controller/ForumPostController.java
@@ -11,7 +11,7 @@ import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api/forum")
-@CrossOrigin(origins = "http://localhost:3000")
+
 public class ForumPostController {
 
     @Autowired
