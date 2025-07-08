ac684a0cf0ad435fa46df7b0523cab9ab6c47a01
Aditya yadav <125307797+Doit-Ay@users.noreply.github.com>
2025-07-08 14:01:33 +0530

    Update ForumPostController.java

--- src/main/java/com/oceansense/controller/ForumPostController.java
+++ src/main/java/com/oceansense/controller/ForumPostController.java
@@ -5,43 +5,43 @@ import com.oceansense.service.ForumPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequestMapping("/api/forum")
-@CrossOrigin(origins = "http://localhost:3000") // Adjust origin as per your frontend setup
+@CrossOrigin(origins = "http://localhost:3000")
 public class ForumPostController {
 
     @Autowired
     private ForumPostService forumPostService;
 
-    // Get all forum posts
     @GetMapping("/posts")
-    public ResponseEntity<List<ForumPost>> getAllPosts() {
+    public ResponseEntity<List<ForumPost>> getAllPosts() throws ExecutionException, InterruptedException {
         List<ForumPost> posts = forumPostService.getAllPosts();
         return ResponseEntity.ok(posts);
     }
 
-    // Create a new forum post
     @PostMapping("/create")
-    public ResponseEntity<ForumPost> createPost(@RequestBody ForumPost post) {
+    public ResponseEntity<ForumPost> createPost(@RequestBody ForumPost post) throws ExecutionException, InterruptedException {
         ForumPost createdPost = forumPostService.createPost(post);
         return ResponseEntity.ok(createdPost);
     }
 
-    // Delete a forum post by ID
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+    public ResponseEntity<Void> deletePost(@PathVariable String id) throws ExecutionException, InterruptedException {
         forumPostService.deletePost(id);
         return ResponseEntity.noContent().build();
     }
 
-    // Get a single forum post by ID
     @GetMapping("/post/{id}")
-    public ResponseEntity<ForumPost> getPostById(@PathVariable Long id) {
-        Optional<ForumPost> post = forumPostService.getPostById(id);
-        return post.map(ResponseEntity::ok)
-                   .orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<ForumPost> getPostById(@PathVariable String id) throws ExecutionException, InterruptedException {
+        ForumPost post = forumPostService.getPostById(id);
+        if (post != null) {
+            return ResponseEntity.ok(post);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
